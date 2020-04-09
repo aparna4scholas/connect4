9fb2ed19b2739fff1c600b99e89724d40912b681
aparna4scholas <61361661+aparna4scholas@users.noreply.github.com>
2020-04-09 08:04:13 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
 
+
+var player = 1;
+document.getElementById("currentplayer").innerHTML = player+"";
+
+var data = 
+[
+[0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0]
+]
+console.table(data);
+function hitme(elementId){
+    // alert(elementId);
+    let col = elementId % 10 - 1;
+    let row = getNextAvailableRow(col);
+    
+    data[row][col] = player;
+
+    let elementposition = ''+(row+1)+(col+1);
+    if(player == 1)
+       document.getElementById(elementposition).style.backgroundColor = "red";  
+       else
+       document.getElementById(elementposition).style.backgroundColor = "yellow";
+     
+       if(player == 1){
+           player =2;
+       }else{
+           player =1;
+       }
+       document.getElementById("currentplayer").innerHTML = player;
+
+   console.table(data);
+
+}
+
+function getNextAvailableRow(col){
+   //alert('dddd');
+  for(let row=5;row>=0;row--){
+      if(data[row][col] === 0){
+          return row;
+      }
+  }
+  return 0;
+
+}
